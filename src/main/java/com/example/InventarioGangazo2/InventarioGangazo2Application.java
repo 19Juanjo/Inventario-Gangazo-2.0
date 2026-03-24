6d15b3d6e1385520f967e88882196d1cb28c4b88
19Juanjo <juanmuno1904@gmail.com>
2026-03-24 15:00:40 -0500

    feat: add entitys and configuration yaml

---
+++ src/main/java/com/example/InventarioGangazo2/InventarioGangazo2Application.java
@@ -0,0 +1,13 @@
+package com.example.InventarioGangazo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioGangazo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioGangazo2Application.class, args);
+	}
+
+}
